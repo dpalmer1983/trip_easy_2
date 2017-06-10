6e5ab853010fd90da4beaf7a71d9fff4bf684b64
Doug Palmer <dpalmer@chicagobooth.edu>
2017-06-10 10:48:18 -0500

    username has nilmethod error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :authenticate_user!
-
+    
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -13,5 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :first_name, :last_name])
   end
 
-  
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,12 +45,9 @@ Rails.application.routes.draw do
   # home pages
   root 'trips#index'
 
-
   # READ
   get "/users", :controller => "users", :action => "index"
   get "/users/:id", :controller => "users", :action => "show"
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
