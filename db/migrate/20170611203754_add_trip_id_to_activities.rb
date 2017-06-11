959524ca2f9c898a14d29e0413a0d58f4b7de8fb
Doug Palmer <dpalmer@chicagobooth.edu>
2017-06-11 15:38:31 -0500

    add trip_id to activities

---
+++ db/migrate/20170611203754_add_trip_id_to_activities.rb
@@ -0,0 +1,5 @@
+class AddTripIdToActivities < ActiveRecord::Migration[5.0]
+  def change
+    add_column :activities, :trip_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611140111) do
+ActiveRecord::Schema.define(version: 20170611135655) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20170611140111) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "activity_id"
   end
 
   create_table "users", force: :cascade do |t|
